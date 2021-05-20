49b4b9c82733cd31ea90210eaa994a7dfb87e0e8
shooooori0128 <pf.shori0128@gmail.com>
2021-05-20 19:21:24 +0900

    :sparkles: 出社状況の登録機能の追加

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,12 @@ require('dotenv').config();
   const argDate = process.argv[2];
   const files = await fromFile('todaysjob.txt');
   const mapping = JSON.parse(await fromFile('mapping.json'));
+  const workStyles = JSON.parse(await fromFile('work_style.json'));
 
   const lines = files.split('\n');
 
   // @ts-ignore
-  const [date, ...withoutHeaderLine] = lines;
+  const [date, workStyleName, ...withoutHeaderLine] = lines;
 
   const converted = withoutHeaderLine
     .filter((line) => {
@@ -25,8 +26,10 @@ require('dotenv').config();
 
   // console.log(restCompleted);
 
+  const selectedWorkStyle = workStyles.find(workStyle => workStyle.name === workStyleName);
+
   const summary = summaryMinutesByProject(restCompleted);
   const mappedSummary = mappingProjectCode(summary, mapping);
 
-  miteras(restCompleted, mappedSummary, argDate);
+  miteras(restCompleted, selectedWorkStyle, mappedSummary, argDate);
 })();
